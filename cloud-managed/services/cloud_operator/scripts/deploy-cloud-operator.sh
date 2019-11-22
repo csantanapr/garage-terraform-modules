e4a6f4a7888d24a92f411462c15997e0eefd33a9
Sean Sundberg <seansund@us.ibm.com>
2019-11-21 21:33:45 -0600

    Suppress output for ibmcloud login

--- cloud-managed/services/cloud_operator/scripts/deploy-cloud-operator.sh
+++ cloud-managed/services/cloud_operator/scripts/deploy-cloud-operator.sh
@@ -7,7 +7,7 @@ if [[ -n "${KUBECONFIG_IKS}" ]]; then
     export KUBECONFIG="${KUBECONFIG_IKS}"
 fi
 
-ibmcloud login --apikey "${APIKEY}" -r "${REGION}" -g "${RESOURCE_GROUP}"
+ibmcloud login --apikey "${APIKEY}" -r "${REGION}" -g "${RESOURCE_GROUP}" 1> /dev/null 2> /dev/null
 
 export IC_APIKEY="${APIKEY}"
 curl -sL https://raw.githubusercontent.com/IBM/cloud-operators/master/hack/install-operator.sh | bash
