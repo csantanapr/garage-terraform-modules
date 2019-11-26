5e0b7e3aaed502b1fe5aca043203c1ed1c5ba991
Sean Sundberg <seansund@us.ibm.com>
2019-11-25 21:24:05 -0600

    Updates logic for get-secret-value.sh

--- cloud-managed/services/postgres/scripts/get-secret-value.sh
+++ cloud-managed/services/postgres/scripts/get-secret-value.sh
@@ -4,4 +4,4 @@ NAME="$1"
 NAMESPACE="$2"
 KEY="$3"
 
-kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o jsonpath="{.data.${KEY}}" | base64 -D | xargs -I{} echo -n {}
+kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o "jsonpath={.data.${KEY}}" | base64 -D | xargs -I{} echo -n {}
