9922cec76da6a6ffd431c9cdb450eae3b85d8643
Sean Sundberg <seansund@us.ibm.com>
2019-11-25 23:17:38 -0600

    Test

--- cloud-managed/services/logdna/scripts/get-secret-value.sh
+++ cloud-managed/services/logdna/scripts/get-secret-value.sh
@@ -4,5 +4,5 @@ NAME="$1"
 NAMESPACE="$2"
 KEY="$3"
 
-kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o yaml
-kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o "jsonpath={.data.${KEY}}" | base64 -D | xargs -I{} echo -n {}
+kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o json
+kubectl get "secret/${NAME}" -n "${NAMESPACE}" -o "jsonpath={.data.${KEY}}" | base64 -D
