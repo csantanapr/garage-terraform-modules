256e3559b412dfd8d26322cf90886d3c68b18808
Sean Sundberg <seansund@us.ibm.com>
2019-12-11 11:02:57 -0600

    OLM: Adds logic to skip install on OpenShift 4.x

--- self-managed/software/operator-lifecycle-manager/main.tf
+++ self-managed/software/operator-lifecycle-manager/main.tf
@@ -1,4 +1,8 @@
+
+
 resource "null_resource" "deploy_operator_lifecycle_manager" {
+  count = "${replace(var.clusterVersion, "/([0-9]).*/", "$1") == "4" ? 0 : 1 }"
+
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-olm.sh ${var.olm_version}"
 
--- self-managed/software/operator-lifecycle-manager/variables.tf
+++ self-managed/software/operator-lifecycle-manager/variables.tf
@@ -1,4 +1,14 @@
 
+variable "clusterType" {
+  type        = "string"
+  description = "The type of cluster (openshift or kubernetes)"
+}
+
+variable "clusterVersion" {
+  type        = "string"
+  description = "The version of kubernetes/openshift of the cluster"
+}
+
 variable "cluster_config_file" {
   type        = "string"
   description = "Cluster config file for Kubernetes cluster."
