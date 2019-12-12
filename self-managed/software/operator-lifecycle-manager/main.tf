9299243d1355654dcd90dc59bebc3156a5720989
Sean Sundberg <seansund@us.ibm.com>
2019-12-12 00:19:25 -0600

    OLM: Test clusterVersion in shell script instead of computed count

--- self-managed/software/operator-lifecycle-manager/main.tf
+++ self-managed/software/operator-lifecycle-manager/main.tf
@@ -1,9 +1,7 @@
 
 resource "null_resource" "deploy_operator_lifecycle_manager" {
-  count = "${replace(var.cluster_version, "/([0-9]).*/", "$1") == "4" ? 0 : 1 }"
-
   provisioner "local-exec" {
-    command = "${path.module}/scripts/deploy-olm.sh ${var.olm_version}"
+    command = "${path.module}/scripts/deploy-olm.sh ${var.olm_version} ${var.cluster_version}"
 
     environment {
       KUBECONFIG_IKS = "${var.cluster_config_file}"
--- self-managed/software/operator-lifecycle-manager/scripts/deploy-olm.sh
+++ self-managed/software/operator-lifecycle-manager/scripts/deploy-olm.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 OLM_VERSION="$1"
+CLUSTER_VERSION="$2"
 
 if [[ -n "${KUBECONFIG_IKS}" ]]; then
-    export KUBECONFIG="${KUBECONFIG_IKS}"
+  export KUBECONFIG="${KUBECONFIG_IKS}"
+fi
+
+if [[ "${CLUSTER_VERSION}" =~ 4[.].* ]]; then
+  echo "Cluster version already has OLM: ${CLUSTER_VERSION}"
+  exit 0
 fi
 
 curl -sL "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${OLM_VERSION}/install.sh" | bash -s "${OLM_VERSION}"
