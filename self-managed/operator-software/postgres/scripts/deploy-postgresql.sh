ba9b70dd57301ca55017a23e1cc9cc1a92259ed4
Sean Sundberg <seansund@us.ibm.com>
2019-12-12 21:41:10 -0600

    Postgres operator: Updates logic to patch deployment

--- self-managed/operator-software/postgres/scripts/deploy-postgresql.sh
+++ self-managed/operator-software/postgres/scripts/deploy-postgresql.sh
@@ -28,12 +28,18 @@ kubectl create -f https://operatorhub.io/install/postgres-operator.yaml
 kubectl apply -f "${MODULE_DIR}/manifests/postgresql-operator-configuration.yaml" -n "${OPERATOR_NAMESPACE}"
 
 # Patch the deployment to include env variable pointing to configuration
-kubectl patch deployents/postgres-operator \
-  -n "${OPERATOR_NAMESPACE}" \
-  --type json \
-  -p '[{"op": "add", "path": "/spec/containers/env/0", "value": {"name": "POSTGRES_OPERATOR_CONFIGURATION_OBJECT", "value": "postgresql-operator-configuration"}}]'
+if [[ -z $(kubectl get deployments/postgres-operator -n operators -o jsonpath='{.spec.template.spec.containers[0].env[0].name}') ]]; then
+  echo "Patching deploymemt"
+  kubectl patch deployents/postgres-operator \
+    -n "${OPERATOR_NAMESPACE}" \
+    --type='json' \
+    -p='[{"op": "add", "path": "/spec/template/spec/containers/0/env", "value": [{"name": "POSTGRES_OPERATOR_CONFIGURATION_OBJECT", "value": "postgresql-operator-configuration"}]}]'
 
-# Delete the pod?
+  # Delete the pod(s) to pick up the change
+  kubectl get pods -l name=postgres-operator -n "${OPERATOR_NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | while read -r pod_name; do
+    kubectl delete pod "${pod_name}" -n "${OPERATOR_NAMESPACE}"
+  done
+fi
 
 until kubectl get pod -l name=postgres-operator -n "${OPERATOR_NAMESPACE}"; do
   echo "Postgresql operator available"
