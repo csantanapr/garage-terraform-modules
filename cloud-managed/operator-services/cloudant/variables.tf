04d1bcf57df243e9b6e278b06a3e9a06eda6cb06
Sean Sundberg <seansund@us.ibm.com>
2020-02-06 23:20:04 -0600

    Upates varialbe types for terraform 0.12

--- cloud-managed/operator-services/cloudant/variables.tf
+++ cloud-managed/operator-services/cloudant/variables.tf
@@ -1,51 +1,51 @@
 variable "resource_group_name" {
-  type        = "string"
+  type        = string
   description = "Resource group where the cluster has been provisioned."
 }
 
 variable "resource_location" {
-  type        = "string"
+  type        = string
   description = "Geographic location of the resource (e.g. us-south, us-east)"
 }
 
 variable "cluster_id" {
-  type        = "string"
+  type        = string
   description = "Id of the cluster"
 }
 
 variable "dev_namespace" {
-  type        = "string"
+  type        = string
   description = "Development namespace"
 }
 
 variable "test_namespace" {
-  type        = "string"
+  type        = string
   description = "Test namespace"
 }
 
 variable "staging_namespace" {
-  type        = "string"
+  type        = string
   description = "Staging namespace"
 }
 
 variable "name_prefix" {
-  type        = "string"
+  type        = string
   description = "The prefix name for the service. If not provided it will default to the resource group name"
   default     = ""
 }
 
 variable "plan" {
-  type        = "string"
+  type        = string
   description = "The type of plan the service instance should run under (lite or standard)"
   default     = "standard"
 }
 
 variable "service_namespace" {
-  type        = "string"
+  type        = string
   description = "The namespace where the service obj will be created"
 }
 
 variable "cluster_config_file" {
-  type        = "string"
+  type        = string
   description = "Cluster config file for Kubernetes cluster."
 }
--- cloud-managed/services/cloud_object_storage/variables.tf
+++ cloud-managed/services/cloud_object_storage/variables.tf
@@ -1,42 +1,42 @@
 variable "resource_group_name" {
-  type        = "string"
+  type        = string
   description = "Resource group where the cluster has been provisioned."
 }
 
 variable "resource_location" {
-  type        = "string"
+  type        = string
   description = "Geographic location of the resource (e.g. us-south, us-east)"
   default     = "global"
 }
 
 variable "cluster_id" {
-  type        = "string"
+  type        = string
   description = "Id of the cluster"
 }
 
 variable "dev_namespace" {
-  type        = "string"
+  type        = string
   description = "Development namespace"
 }
 
 variable "test_namespace" {
-  type        = "string"
+  type        = string
   description = "Test namespace"
 }
 
 variable "staging_namespace" {
-  type        = "string"
+  type        = string
   description = "Staging namespace"
 }
 
 variable "name_prefix" {
-  type        = "string"
+  type        = string
   description = "The prefix name for the service. If not provided it will default to the resource group name"
   default     = ""
 }
 
 variable "plan" {
-  type        = "string"
+  type        = string
   description = "The type of plan the service instance should run under (lite or standard)"
   default     = "standard"
 }
