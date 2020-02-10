834d0a6982493575a9217af1d22ca840d476afc5
Sean Sundberg <seansund@us.ibm.com>
2020-02-09 23:08:06 -0600

    Removes kubernetes provider reference

--- self-managed/operator-software/cloud-operator/main.tf
+++ self-managed/operator-software/cloud-operator/main.tf
@@ -4,58 +4,15 @@ locals {
   operator-source = var.cluster_type == "ocp4" ? "community-operators" : "operatorhubio-catalog"
 }
 
-provider "kubernetes" {
-  host     = var.server_url
-
-  username = var.login_user
-  password = var.login_password
-}
-
-resource "kubernetes_secret" "seed-secret" {
-  metadata {
-    name = "seed-secret"
-    labels = {
-      "seed.ibm.com/ibmcloud-token" = "apikey"
-      "app.kubernetes.io/name"      = "ibmcloud-operator"
-    }
-  }
-
-  data = {
-    api-key = var.ibmcloud_api_key
-    region  = var.resource_location
-  }
-
-  type = "Opaque"
-}
-
-resource "kubernetes_config_map" "example" {
-  metadata {
-    name = "seed-defaults"
-    labels = {
-      "app.kubernetes.io/name" = "ibmcloud-operator"
-    }
-  }
-
-  data = {
-    region        = var.resource_location
-    resourceGroup = var.resource_group_name
-    org           = ""
-    space         = ""
-  }
-}
-
 resource "null_resource" "deploy_cloud_operator" {
-  depends_on = [
-    kubernetes_secret.seed-secret,
-    kubernetes_config_map.example,
-  ]
-
   provisioner "local-exec" {
-    command = "${path.module}/scripts/deploy-cloud-operator.sh ${var.olm_namespace} ${local.namespace} ${local.operator-source}"
+    command = "${path.module}/scripts/deploy-cloud-operator.sh ${var.resource_location} ${var.olm_namespace} ${local.namespace} ${local.operator-source}"
 
     environment = {
       KUBECONFIG_IKS = var.cluster_config_file
       TMP_DIR        = local.tmp_dir
+      APIKEY         = var.ibmcloud_api_key
+      RESOURCE_GROUP = var.resource_group_name
     }
   }
 }
--- self-managed/operator-software/cloud-operator/scripts/deploy-cloud-operator.sh
+++ self-managed/operator-software/cloud-operator/scripts/deploy-cloud-operator.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 
-SOURCE_NAMESPACE="$1"
-TARGET_NAMESPACE="$2"
-OPERATOR_SOURCE="$3"
+REGION="$1"
+SOURCE_NAMESPACE="$2"
+TARGET_NAMESPACE="$3"
+OPERATOR_SOURCE="$4"
 
 if [[ -n "${KUBECONFIG_IKS}" ]]; then
     export KUBECONFIG="${KUBECONFIG_IKS}"
@@ -13,9 +14,45 @@ if [[ -z "${TMP_DIR}" ]]; then
 fi
 mkdir -p ${TMP_DIR}
 
+if [[ -z "${APIKEY}" ]]; then
+  echo "APIKEY is missing"
+  exit 1
+fi
+
+if [[ -z "${RESOURCE_GROUP}" ]]; then
+  echo "RESOURCE_GROUP is missing"
+  exit 1
+fi
+
 OUTPUT_YAML="${TMP_DIR}/ibmcloud-operator.yaml"
 
 cat <<EOF > "${OUTPUT_YAML}"
+apiVersion: v1
+kind: Secret
+metadata:
+  name: seed-secret
+  labels:
+    seed.ibm.com/ibmcloud-token: "apikey"
+    app.kubernetes.io/name: ibmcloud-operator
+  namespace: default
+type: Opaque
+stringData:
+  api-key: "${APIKEY}"
+  region: "${REGION}"
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: seed-defaults
+  namespace: default
+  labels:
+    app.kubernetes.io/name: ibmcloud-operator
+data:
+  org: ""
+  space: ""
+  region: "${REGION}"
+  resourceGroup: "${RESOURCE_GROUP}"
+---
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
